16d1a3d3b6648277ccfe8fafba634ea6438d800c
anuraq <yoyoanusingh27@gmail.com>
2019-12-18 22:54:04 +0530

    added buffering

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import socket
 
+HEADER = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((socket.gethostname(), 1234))
 
-print(s.recv(1024))
+SIZE = int(s.recv(HEADER).decode('utf-8'))
+
+msg = s.recv(SIZE).decode('utf-8')
+
+print(msg)
--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 import socket
 
+HEADER = 10
+
+HOST = socket.gethostname()
+PORT = 1234
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind((socket.gethostname(), 1234))
+s.bind((HOST, PORT))
 s.listen(5)
+msg = "Welcome to server!!"
+msg = f'{len(msg):<{HEADER}}' + msg
 
 while True:
     conn, address = s.accept()
     print(f"new connection formed at address: {address}")
-    conn.send("Welcome to server!!")
+    conn.send(bytes(msg,'utf-8'))
+    conn.close()

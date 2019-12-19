49e9cfd0e7cb5a006bd048d3bad081223f7ff4d2
anuraq <yoyoanusingh27@gmail.com>
2019-12-19 21:20:51 +0530

    server as thread and exit statement

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import socket
+import time
+import threading
+import keyboard
 
 HEADER = 10
 
@@ -11,8 +14,29 @@ s.listen(5)
 msg = "Welcome to server!!"
 msg = f'{len(msg):<{HEADER}}' + msg
 
+def server_socket_running():
+    global STOP_SERVER
+    while True:
+        if STOP_SERVER:
+            print("server break")
+            break
+        conn, address = s.accept()
+        print(f"new connection formed at address: {address}")
+        conn.send(bytes(msg,'utf-8'))
+        conn.close()
+
+STOP_SERVER = False
+x = threading.Thread(target=server_socket_running, args=())
+
+FIRST = False
+j=0
 while True:
-    conn, address = s.accept()
-    print(f"new connection formed at address: {address}")
-    conn.send(bytes(msg,'utf-8'))
-    conn.close()
+    if not FIRST:
+        x.start()
+        FIRST = True
+    print('waiting : '+str(j), end='\r')
+    time.sleep(1)
+    j += 1
+    if keyboard.is_pressed('q'):
+        STOP_SERVER = True
+        break

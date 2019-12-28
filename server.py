93b254deff428cc4946f6b8189a10f60fc9dc904
anuraq <yoyoanusingh27@gmail.com>
2019-12-28 22:40:26 +0530

    modified code to class based approach

--- server.py
+++ server.py
@@ -1,51 +1,52 @@
 import socket
 import time
 import threading
-#import keyboard
 from pynput.keyboard import Listener
 
 HEADER = 10
-
 HOST = socket.gethostname()
 PORT = 1234
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind((HOST, PORT))
-s.listen(5)
-msg = "Welcome to server!!"
-msg = f'{len(msg):<{HEADER}}' + msg
+class Server():
+    def __init__(self, header, addr):
+        self.h = header
+        self.a = addr
 
-def server_socket_running():
-    while True:
-        conn, address = s.accept()
-        print(f"new connection formed at address: {address}")
-        conn.send(bytes(msg,'utf-8'))
-        conn.close()
+    def start_server(self):
+        def server_socket_running(HEADER, addr):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(addr)
+            s.listen(5)
+            msg = "Welcome to server!!"
+            msg = f'{len(msg):<{HEADER}}' + msg
+            while True:
+                conn, address = s.accept()
+                print(f"new connection formed at address: {address}")
+                conn.send(bytes(msg,'utf-8'))
+                conn.close()
 
+        def is_pressed(key):
+            nonlocal SERVER_EXIT
+            try:
+                if key.char == 'q':
+                    SERVER_EXIT = True
+            except AttributeError:
+                pass
 
-def is_pressed(key):
-    global EXIT
-    try:
-        if key.char == 'q':
-            EXIT = True
-    except AttributeError:
-        pass
+        x = threading.Thread(target=server_socket_running, args=(self.h, self.a), daemon=True)
+        k = Listener(on_press=is_pressed, daemon=True)
 
-
-x = threading.Thread(target=server_socket_running, args=(), daemon=True)
-k = Listener(on_press=is_pressed, daemon=True)
-k.start()
-
-EXIT = False
-FIRST = False
-j=0
-while True:
-    if not FIRST:
-        x.start()
-        print("Press 'q' for stopping the server.")
-        FIRST = True
-    print('Server running for : '+str(j)+' seconds', end='\r')
-    time.sleep(1)
-    j += 1
-    if EXIT:
-        break
+        SERVER_EXIT = False
+        FIRST = False
+        j=0
+        k.start()
+        while True:
+            if not FIRST:
+                x.start()
+                print("Press 'q' for stopping the server.")
+                FIRST = True
+            print('Server running for : '+str(j)+' seconds', end='\r')
+            time.sleep(1)
+            j += 1
+            if SERVER_EXIT:
+                break
